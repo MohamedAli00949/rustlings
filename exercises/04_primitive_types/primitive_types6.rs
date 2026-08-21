d8b8221ceae748326bdabac628fc69a7544a7baf
MohamedAli00949 <76850143+MohamedAli00949@users.noreply.github.com>
2026-08-21 23:41:40 +0300

    feat: solve the primitive types exercies

--- exercises/04_primitive_types/primitive_types6.rs
+++ exercises/04_primitive_types/primitive_types6.rs
@@ -12,6 +12,7 @@ mod tests {
         // and assign it to a variable called `second`.
         // let second = ???;
 
+        let (_, second, _) = numbers; // This is exception syntax for tuple with 3 items.
         assert_eq!(second, 2, "This is not the 2nd number in the tuple!");
     }
 }
