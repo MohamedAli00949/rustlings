3a1e5008ab091d6a41020416ad9c48d396f1babb
MohamedAli00949 <76850143+MohamedAli00949@users.noreply.github.com>
2026-08-28 01:55:48 +0300

    solve: trait exercises

--- exercises/15_traits/traits1.rs
+++ exercises/15_traits/traits1.rs
@@ -6,6 +6,17 @@ trait AppendBar {
 
 impl AppendBar for String {
     // TODO: Implement `AppendBar` for the type `String`.
+    fn append_bar(self) -> Self {
+        // !!! bad solution
+        // let mut result = self;
+
+        // result.push_str("Bar");
+
+        // result
+
+        // *** The best solution
+        self + "Bar"
+    }
 }
 
 fn main() {
