5a4e6e93223ec61837f3c7719b546d52002b955e
MohamedAli00949 <76850143+MohamedAli00949@users.noreply.github.com>
2026-08-31 01:37:36 +0300

    solve: macros exercises

--- exercises/21_macros/macros3.rs
+++ exercises/21_macros/macros3.rs
@@ -1,6 +1,9 @@
 // TODO: Fix the compiler error without taking the macro definition out of this
 // module.
+#[macro_use]
 mod macros {
+    // OR another method
+    // #[macro_export]
     macro_rules! my_macro {
         () => {
             println!("Check out my macro!");
